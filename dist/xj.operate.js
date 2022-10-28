bbfb57ad9de7c4487e1cbb54f197175e549f2e94
xjArea <xjArea@qq.com>
2022-10-29 03:07:25 +0800

    0.6.0

--- dist/xj.operate.js
+++ dist/xj.operate.js
@@ -1,4 +1,4 @@
-/** xj.operate(判断操作方式) | V0.5.0 | Apache Licence 2.0 | 2016-2021 © XJ.Chen | https://github.com/xjZone/xj.operate */
+/** xj.operate(判断操作方式) | V0.6.0 | Apache Licence 2.0 | 2016-2022 © XJ.Chen | https://github.com/xjZone/xj.operate/ */
 ;(function(global, factory){
 	if(typeof(define) === 'function' && (define.amd !== undefined || define.cmd !== undefined)){ define(factory) }
 	else if(typeof(module) !== 'undefined' && typeof(exports) === 'object'){ module.exports = factory() }
@@ -7,31 +7,25 @@
 
 
 
-// Polyfill : classList V0.1.1
-// 解决 IE10 的 SVG 标签不支持 classList 对象的 3 个方法 : pub_hasClass() / pub_addClass() / pub_delClass()
-var pub_hasClass,pub_addClass,pub_delClass;!function(){pub_hasClass=function(a,b){var c,d;return a.classList?a.classList.contains(b):(c=a.getAttribute("class"),d=c?c.split(/\s+/):[],-1===d.indexOf(b)?!1:!0)},pub_addClass=function(a,b){var c,d;return a.classList?a.classList.add(b):(c=a.getAttribute("class"),d=c?c.split(/\s+/):[],0===d.length?a.setAttribute("class",b):-1===d.indexOf(b)&&a.setAttribute("class",d.join(" ")+" "+b),void 0)},pub_delClass=function(a,b){var c,d;return a.classList?a.classList.remove(b):(c=a.getAttribute("class"),d=c?c.split(/\s+/):[],c="",-1!==d.indexOf(b)&&(d.forEach(function(a){a!==b&&(c+=a+" ")}),a.setAttribute("class",c.trim())),void 0)}}();
-
-
-
 // ---------------------------------------------------------------------------------------------
 // globalThis | window | self | global
 var pub_global = (typeof(globalThis) !== 'undefined' ? globalThis : typeof(window) !== 'undefined' ? window : typeof(self) !== 'undefined' ? self : global);
 
 // public nothing, version, keyword
-var pub_nothing = function(){}, pub_version = '0.5.0', pub_keyword = 'operate';
+var pub_nothing = function(){}, pub_version = '0.6.0', pub_keyword = 'operate';
 
 // public config, advance set
 var pub_config = {
 	
-	classTarget : document.documentElement,		// 将被添加 existClass / mouseClass / touchClass / otherClass 类名的目标节点，默认是 html 标签，不推荐修改，保持默认即可
-	existClass : 'xj-operate-exist',			// 初始化后 targetClass 元素节点会被添加的类名，默认是 'xj-operate-exist'，可用于 CSS 判断环境中是否存在 xj.operate 插件
+	classTarget : document.documentElement,		// 将被添加 existClass | mouseClass | touchClass | otherClass 类名的节点，默认 html 标签，建议保持默认别修改
+	existClass : 'xj-operate-exist',			// 初始化后 classTarget 会被添加的类名，默认是 'xj-operate-exist'，可用于 CSS 判断是否有存在 xj.operate 插件
 	
-	mouseClass : 'xj-operate-mouse',			// 当使用了鼠标进行操作，会在 classTarget 节点上添加的类名，默认为 'xj-operate-mouse'，并不推荐修改，因为不少 xj 插件都是默认只响应这个类名，如果需要配置其他类名，可在 mouseCallback 回调中设置
-	touchClass : 'xj-operate-touch',			// 当使用了触屏进行操作，会在 classTarget 节点上添加的类名，默认为 'xj-operate-touch'，并不推荐修改，因为不少 xj 插件都是默认只响应这个类名，如果需要配置其他类名，可在 touchCallback 回调中设置
-	otherClass : 'xj-operate-other',			// 既不是鼠标也不是触屏，会在 classTarget 节点上添加的类名，默认为 'xj-operate-other'，但实际上这个状态并不会自动出现，页面只会在 mouse 和 touch 这两个状态中进行切换，除非是主动调用 otherSet()
+	mouseClass : 'xj-operate-mouse',			// 当使用了鼠标进行操作，会在 classTarget 节点上添加的类名，默认是 'xj-operate-mouse'，并不推荐修改，因为不少 xj 插件都是默认只响应这个类名，如果需要配置其他类名，可在 mouseCallback 回调中设置
+	touchClass : 'xj-operate-touch',			// 当使用了触屏进行操作，会在 classTarget 节点上添加的类名，默认是 'xj-operate-touch'，并不推荐修改，因为不少 xj 插件都是默认只响应这个类名，如果需要配置其他类名，可在 touchCallback 回调中设置
+	otherClass : 'xj-operate-other',			// 既不是鼠标也不是触屏，会在 classTarget 节点上添加的类名，默认是 'xj-operate-other'，但实际上这个状态并不会自动出现，页面只会在 mouse 和 touch 这两个状态中进行切换，除非是主动调用 otherSet()
 	
-	frequency : 10,								// mousemove 事件的触发频率，默认为 10，在 duration 参数这个时间内，能连续触发这么多次 mousemove，那就算是使用了鼠标设备
-	duration : 1000,							// mousemove 事件的触发时间，默认为 1000(ms)，在这个时间内能连续触发 frequency 次 mousemove 事件，那就算是使用了鼠标设备
+	pressHover : false,							// 当处于 touch 状态时，如果长按屏幕且没有移动过，是否临时转遍为 mouse 状态，以便让 hover 生效，默认为 false
+	delayTime : 500,							// touchstart 和 mousemove 的最大间隔时间，在此时间内触发连续触发这两个事件，将会进入 mouse 状态，默认为 500
 	
 	mouseCallback : pub_nothing,				// 触发为 mouse 状态的回调函数，temporary 参数是这个状态是否为临时性转遍的标志，一般都是 true，除非调用 mouseSet() 时传入了 false，如果该函数最终返回的是 false，那么将会阻止本次进入 mouse 状态
 	touchCallback : pub_nothing,				// 触发为 touch 状态的回调函数，temporary 参数是这个状态是否为临时性转遍的标志，一般都是 true，除非调用 touchSet() 时传入了 false，如果该函数最终返回的是 false，那么将会阻止本次进入 touch 状态
@@ -69,44 +63,151 @@ var pub_body = pub_doc.body;
 
 
 // ---------------------------------------------------------------------------------------------
-// mouse 事件监听函数将会用到的变量
-var lastTime = 0;
-var thisTime = Date.now();
-var totalDuration = 0;
-var durationArray = new Array();
+// V0.5.X- 判断，原理是通过触屏环境无法在短时间内连续触发多次 mousemove 实现，但那样响应不够灵敏
+// V0.6.X+ 判断，改为 mousemove 触发和 touchstart 触发，其间隔是否小于 500ms，逻辑复杂但是更灵敏
 
+// Safari(IOS) 长按时只会触发 touchstart 和 touchend，不会触发 mousemove，但可通过 touchend 判断
+// touchend 和 touchstart 的触发时间距大于 500ms，那就算是长按，因为一般长按的响应时间就是 500ms
 
+// Android 的 WX 和 UC，长按只会触发 touchstart 事件，因为长按所弹出的菜单，会导致其他事件被中断
+// 所以无法通过 touchend 等事件来判断是否为长按，但好在会触发 touchcancel 事件，可绑定该事件判断
+// Android 的 Firefox 长按也可能触发 touchmove 事件，触发 touchmove 事件意味着这是滚动或缩放操作
+// 所以最终的逻辑改为即使触发了 touchmove，但只要触发次数少于 2 次，那也还是可能处于长按的操作中
 
-// touch 事件监听函数，touch = true
-var touchListener = function(event){
-	if(event && event.isTrusted === false
-	){ return }else{ pub_return.touchSet() };
+// Chrome 的移动模拟，长按后放开会先触发 touchend 后触发 mousemove，所以会先执行 touchend 的回调
+// 如果允许进入 mouse 状态，那么在 touchend 回调中就会进入了，之后 mousemove 回调将会直接 return
+
+// 最大问题是 mouse 临时换成 touch，之后就无法再自动恢复成 mouse 状态，因为此时 mousemove 超时了
+// 如果遇到 pressHover === false 时会 return，所以创建 trulyTouchstart 判断到能 touch 就不要继续
+// 用 'ontouchstart' in document 判断是不行的，Chrome 切换状态或触屏设备拔插鼠标后该判断就不准了
+// 所以改为在 touchstart 事件中使用 trulyTouchstart，来动态的真实判断当前环境是否支持 touch 事件
+
+var trulyTouchstart = false;   // 是否真实触发过
+var lastTouchstart = 0;        // 触屏开始的时间
+var hasTouchstart = false;     // 是否有触屏开始
+var touchmoveSum = 0;          // 触屏移动的次数
+var hasTouchend = false;       // 是否有触屏结束
+
+// 当 mousemove 没紧跟 touchstart 触发就算是鼠标
+// 所谓紧跟就是两个事件的触发间隔值 >= delayTime
+var mousemoveListener = function(event){
+	
+	if(event.isTrusted === false){ return }
+	else if(pub_return.mouse === true){ return }
+	else if(touchmoveSum > 2){ return 'scroll/zoom' };
+	
+	if(Date.now() - lastTouchstart <= pub_config.delayTime){ return }
+	else if(Date.now() - lastTouchstart  >  pub_config.delayTime && 
+	pub_config.pressHover === false && trulyTouchstart === true){
+		trulyTouchstart = false;
+		return;
+	};
+	
+	pub_return.mouseSet();
+	
 };
 
+// 触发了 touchstart，就更新 lastTouchstart 变量
+// 点击时间和 touch 判断不能颠倒，时间后面要用到
+var touchstartListener = function(event){
+	
+	if(event.isTrusted === false){ return };
+	if(event.touches.length > 1){ return };
+	
+	if(touchmoveSum > 2){ return };
+	hasTouchstart = true, hasTouchend = false;
+	
+	trulyTouchstart = true;
+	
+	lastTouchstart = Date.now( );
+	if(pub_return.touch === true){ return };
+	
+	pub_return.touchSet();
+	
+};
 
+// 触发 touchmove 就是滚动或缩放，所有操作都作废
+// Firefox(Android) 长按也会触发，所以改为累进值
+var touchmoveListener = function(event){
+	if(event.isTrusted === true){
+		touchmoveSum += 1;
+	};
+};
 
-// mouse 事件监听函数，mouse = true
-var mouseListener = function(event){
+// touchend 距离 touchstart 超过 500ms，就算长按
+// 当手指都离开后 hasTouchstart 等变量才能初始化
+var touchendListener = function(event){
 	
-	if(event && event.isTrusted === false){ return };
+	if(event.isTrusted === false){ return };
+	if(event.touches.length > 0){ return };
 	
-	lastTime = thisTime;
-	thisTime = new Date().getTime();
+	if(pub_config.pressHover === true 
+	&& Date.now() - lastTouchstart >= 500 && 
+	touchmoveSum <= 2 && pub_return.mouse === false 
+	){ pub_return.mouseSet() };
 	
-	durationArray.push( thisTime - lastTime );
-	if(durationArray.length < pub_config.frequency){ return };
+	hasTouchstart = false;
+	touchmoveSum = 0;
+	hasTouchend = true;
 	
-	durationArray.forEach(function(time){ totalDuration += time });
-	if(totalDuration <= pub_config.duration){ pub_return.mouseSet() };
+};
+
+// 类似 touchend，但是有开始却没有结束，也算长按
+// touchcancel 时，event.touches.length 也是为 0
+var touchcancelListener = function(event){
 	
-	totalDuration = 0;
-	durationArray = [];
+	if(event.isTrusted === false){ return };
+	if(event.touches.length > 0){ return };
+	
+	if(pub_config.pressHover === true 
+	&& Date.now() - lastTouchstart >= 500 && 
+	touchmoveSum <= 2 && pub_return.mouse === false 
+	){ if(hasTouchstart === true 
+	&& hasTouchend === false){ pub_return.mouseSet() }; };
+	
+	hasTouchstart = false;
+	touchmoveSum = 0;
+	hasTouchend = true;
 	
 };
 
 
 
 // ---------------------------------------------------------------------------------------------
+// 不用担心重复绑定，因为重复会无效
+var toggleEventListener = function(temporary){
+	
+	pub_doc[temporary === true ? 'addEventListener' : 
+	'removeEventListener']('mousemove',   mousemoveListener,   true);
+	
+	pub_doc[temporary === true ? 'addEventListener' : 
+	'removeEventListener']('touchstart',  touchstartListener,  true);
+	
+	pub_doc[temporary === true ? 'addEventListener' : 
+	'removeEventListener']('touchmove',   touchmoveListener,   true);
+	
+	pub_doc[temporary === true ? 'addEventListener' : 
+	'removeEventListener']('touchend',    touchendListener,    true);
+	
+	pub_doc[temporary === true ? 'addEventListener' : 
+	'removeEventListener']('touchcancel', touchcancelListener, true);
+	
+};
+
+// 根据传入的状态值来决定类名的设置
+var switchTargetClass = function(state){
+	
+	pub_config.classTarget.classList[state === 
+	'mouse' ? 'add' : 'remove'](pub_config.mouseClass);
+	
+	pub_config.classTarget.classList[state === 
+	'touch' ? 'add' : 'remove'](pub_config.touchClass);
+	
+	pub_config.classTarget.classList[state === 
+	'other' ? 'add' : 'remove'](pub_config.otherClass);
+	
+};
+
 // 这是插件执行后最终将会返回的对象
 var pub_return = {
 	
@@ -116,99 +217,54 @@ var pub_return = {
 	touch : false,								// 是否正处于 touch 状态，是则返回 true，否则返回 fasle
 	other : false,								// 是否正处于 other 状态，是则返回 true，否则返回 fasle，实际上这个状态并不会自动出现，除非是手动调用了 otherSet()
 	
-	mouseSet : pub_nothing,						// 手动设置为 mouse 状态，可传入 temporary 参数，不传则默认为 true，也就是设置为 mouse 状态是临时的，传入 false 则永久性进入 mouse 状态，之后就得用 mouseSet() / touchSet() 方法才能恢复继续监听
-	touchSet : pub_nothing,						// 手动设置为 touch 状态，可传入 temporary 参数，不传则默认为 true，也就是设置为 touch 状态是临时的，传入 false 则永久性进入 touch 状态，之后就得用 mouseSet() / touchSet() 方法才能恢复继续监听
-	otherSet : pub_nothing,						// 手动设置为 other 状态，可传入 temporary 参数，不传则默认为 true，也就是设置为 other 状态是临时的，传入 false 则永久性进入 other 状态，之后就得用 mouseSet() / touchSet() 方法才能恢复继续监听
+	mouseSet : pub_nothing,						// 手动设置为 mouse 状态，可传入 temporary 参数，不传则默认是 true，也就是设置状态是临时的，传入 false 则永久性进入 mouse 状态，得再次调用 mouseSet() | touchSet() | otherSet() 才能恢复继续监听
+	touchSet : pub_nothing,						// 手动设置为 touch 状态，可传入 temporary 参数，不传则默认是 true，也就是设置状态是临时的，传入 false 则永久性进入 touch 状态，得再次调用 mouseSet() | touchSet() | otherSet() 才能恢复继续监听
+	otherSet : pub_nothing,						// 手动设置为 other 状态，可传入 temporary 参数，不传则默认是 true，也就是设置状态是临时的，传入 false 则永久性进入 other 状态，得再次调用 mouseSet() | touchSet() | otherSet() 才能恢复继续监听
 	
 };
 
 pub_return.mouseSet = function(temporary){
 	
-	lastTime = thisTime;
-	thisTime = Date.now();
-	totalDuration = 0;
-	durationArray = [];
-	
 	if(temporary === undefined){ temporary = true };
 	
-	if(pub_config.mouseCallback !== pub_nothing && 
-	pub_config.mouseCallback(temporary) === false){ return };
+	if(pub_config.mouseCallback(temporary) === false){ return };
 	
 	pub_return.mouse = true;
 	pub_return.touch = false;
 	pub_return.other = false;
 	
-	pub_addClass(pub_config.classTarget, pub_config.mouseClass);
-	pub_delClass(pub_config.classTarget, pub_config.touchClass);
-	pub_delClass(pub_config.classTarget, pub_config.otherClass);
-	
-	if(temporary === true){
-		pub_doc.removeEventListener('mousemove', mouseListener, true);
-		pub_doc.addEventListener('touchstart', touchListener, true);
-	}else{
-		pub_doc.removeEventListener('mousemove', mouseListener, true);
-		pub_doc.removeEventListener('touchstart', touchListener, true);
-	};
+	switchTargetClass('mouse');
+	toggleEventListener(temporary);
 	
 };
 
 pub_return.touchSet = function(temporary){
 	
-	lastTime = thisTime;
-	thisTime = Date.now();
-	totalDuration = 0;
-	durationArray = [];
-	
 	if(temporary === undefined){ temporary = true };
 	
-	if(pub_config.touchCallback !== pub_nothing && 
-	pub_config.touchCallback(temporary) === false){ return };
+	if(pub_config.touchCallback(temporary) === false){ return };
 	
 	pub_return.mouse = false;
 	pub_return.touch = true;
 	pub_return.other = false;
 	
-	pub_delClass(pub_config.classTarget, pub_config.mouseClass);
-	pub_addClass(pub_config.classTarget, pub_config.touchClass);
-	pub_delClass(pub_config.classTarget, pub_config.otherClass);
-	
-	if(temporary === true){
-		pub_doc.addEventListener('mousemove', mouseListener, true);
-		pub_doc.removeEventListener('touchstart', touchListener, true);
-	}else{
-		pub_doc.removeEventListener('mousemove', mouseListener, true);
-		pub_doc.removeEventListener('touchstart', touchListener, true);
-	};
+	switchTargetClass('touch');
+	toggleEventListener(temporary);
 	
 };
 
 pub_return.otherSet = function(temporary){
 	
-	lastTime = thisTime;
-	thisTime = Date.now();
-	totalDuration = 0;
-	durationArray = [];
-	
 	if(temporary === undefined){ temporary = true };
 	
-	if(pub_config.otherCallback !== pub_nothing && 
-	pub_config.otherCallback(temporary) === false){ return };
+	if(pub_config.otherCallback(temporary) === false){ return };
 	
 	pub_return.mouse = false;
 	pub_return.touch = false;
 	pub_return.other = true;
 	
-	pub_delClass(pub_config.classTarget, pub_config.mouseClass);
-	pub_delClass(pub_config.classTarget, pub_config.touchClass);
-	pub_addClass(pub_config.classTarget, pub_config.otherClass);
-	
-	if(temporary === true){
-		pub_doc.addEventListener('mousemove', mouseListener, true);
-		pub_doc.addEventListener('touchstart', touchListener, true);
-	}else{
-		pub_doc.removeEventListener('mousemove', mouseListener, true);
-		pub_doc.removeEventListener('touchstart', touchListener, true);
-	};
+	switchTargetClass('other');
+	toggleEventListener(temporary);
 	
 };
 
@@ -216,13 +272,13 @@ pub_return.otherSet = function(temporary){
 
 // ---------------------------------------------------------------------------------------------
 // 添加 existClass 类名
-if(pub_config.classTarget !== null && pub_config.existClass !== ''){ pub_addClass(pub_config.classTarget, pub_config.existClass) };
+if(pub_config.classTarget !== null && pub_config.existClass !== ''){ pub_config.classTarget.classList.add(pub_config.existClass) };
 
 // 初始化状态设置
-if(('ontouchstart' in pub_win) === true){ pub_return.touchSet() }else{ pub_return.mouseSet() };
+if(('ontouchstart' in pub_doc) === true){ pub_return.touchSet() }else{ pub_return.mouseSet() };
 
 // 返回对象
-return pub_global.xj.operateReturn[pub_version] = pub_return;
+return pub_global.xj[pub_keyword+'Return'][pub_version] = pub_return;
 
 
 
--- readme.md
+++ readme.md
@@ -1,14 +1,19 @@
 <!--◇ ----------------------------------------------------------------------------------- ◇-->
-# xj.operate(判断操作方式)
+# xj.operate(判断操作方式)  
 
-**简介** : 这个插件主要用于判断用户的操作方式，究竟是用鼠标操作还是触屏操作，在引入这个插件之后，当用户使用鼠标进行操作时，`<html>` 上将会出现 `xj-operate-mouse` 的类，当用户使用触屏进行操作时，`<html>` 上将会出现 `xj-operate-touch` 的类，这两个类名的设置并非是一次性的，它会根据用户的操作模式进行动态监听和变化，这有助于我们根据用户不同的操作方式进行不同的设置。
+**简介** : 这个插件主要用于判断用户的操作方式，究竟是用鼠标操作还是触屏操作，在引入了这个插件之后，当用户使用鼠标进行操作时 `<html>` 上将会出现 `xj-operate-mouse` 的类，当用户使用触屏进行操作时 `<html>` 上将会出现 `xj-operate-touch` 的类，这两个类名的设置并非是一次性的，它会根据用户的操作模式进行动态监听和变化，这有助于我们根据用户不同的操作方式进行不同的设置。  
 
-**兼容** : IE10+ / Edge12+ / Firefox / Chrome / Safari / Opera / IOS Webkit / Android Platform
+**兼容** : IE10+ / Edge12+ / Firefox / Chrome / Safari / Opera / IOS Webkit / Android Platform  
 
-**更新** : <https://github.com/xjZone/xj.operate/blob/master/upgrade.md>
+**更新** : <https://github.com/xjZone/xj.operate/blob/master/upgrade.md>  
 
-**文档** : <https://github.com/xjZone/xj.operate/>
+**文档** : <https://xjZone.github.io/xj.operate/>  
 
-**协议** : Apache 2.0
+**协议** : [Apache License, V2.0](https://www.apache.org/licenses/LICENSE-2.0)  
+
+<!-- ————
+
+**推荐阅读 :**  
+XJ.Chen - [浅谈 :hover 伪类选择器在 touch 环境如何禁止生效的问题](https://juejin.cn/post/????)   -->
 
 
